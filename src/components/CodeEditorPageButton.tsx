e16fd7238f7ae7b1043501bacdc659187148ff93
PaulBallestra <paulballestra@gmail.com>
2025-11-15 01:20:58 +0100

    :art: Padding % for pages buttons

--- src/components/CodeEditorPageButton.tsx
+++ src/components/CodeEditorPageButton.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 const CodeEditorPageButton = ({onClick, label, isActive}: {onClick: () => void, label: string, isActive: boolean}) => {
     return (
-        <button onClick={onClick} className={cn("px-2 py-1 cursor-pointer bg-transparent text-white/80 hover:bg-white/10 border-b-1 border-transparent", isActive && "bg-white/80 text-black hover:bg-white/80")}>{label}</button>
+        <button onClick={onClick} className={cn("px-[1vw] py-[.5vh] cursor-pointer bg-transparent text-white/80 hover:bg-white/10 border-b-1 border-transparent", isActive && "bg-white/80 text-black hover:bg-white/80")}>{label}</button>
     )
 }
 
